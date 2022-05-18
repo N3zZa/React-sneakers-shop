1eca592ac4dd371ff6cfd79c096e829257566c32
Nazar <nazar.developer007@gmail.com>
2022-05-18 17:59:07 +0300

    1:52:00

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Content from "./components/Content";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
+
 function App() {
   return (
     <div className="wrapper">
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,20 @@
 
 
-function Card() {
+function Card(props) {
+
   return (
     <div className="card">
       <div className="favourite">
         <img src="/img/whiteheart.svg" alt="heart(unliked)" />
       </div>
-      <img
-        width={133}
-        height={112}
-        src="/img/sneakers/sneakers1.jpg"
-        alt="sneakers"
-      />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
+      <h5>{props.title}</h5>
       <div className="cardInfo">
         <div className="cardPrice">
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
-        <button>
+        <button onClick={props.onClick}>
           <img width={11} height={11} src="/img/plus.svg" alt="plus" />
         </button>
       </div>
